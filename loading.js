3facb41d9e4ccb06cb7745da1d0cfab419afaa6c
klauza <klauza.uk@gmail.com>
2019-07-13 00:41:11 +0100

    add file

---
+++ loading.js
@@ -0,0 +1,16 @@
+const loadingBar = document.querySelector('.progress');
+
+console.log(loadingBar);
+
+window.addEventListener('load', loadFunc);
+
+function loadFunc(){
+  loadingBar.classList.remove('d-none');
+  loadingBar.style.animation = "loadBar 1s forwards";
+  
+  setTimeout(()=>{
+    document.querySelector('.program').classList.remove('d-none');
+    document.querySelector('.program').style.animation = "programOpacity 1200ms forwards";
+    loadingBar.classList.add('d-none');
+  }, 1000)
+}
