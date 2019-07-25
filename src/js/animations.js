4337cec7c53ac619f71372abf3ea2f7f4772bdfc
klauza <klauza.uk@gmail.com>
2019-07-25 20:39:34 +0100

    added icons

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,8 +1,8 @@
 const loadingBar = document.querySelector('.progress');
 
-window.addEventListener('load', loadFunc);
+window.addEventListener('load', loadFunc(moveIcons));
 
-function loadFunc(){
+function loadFunc(callback){
   loadingBar.classList.remove('d-none');
   loadingBar.style.animation = "loadBar 1s forwards";
   
@@ -10,5 +10,17 @@ function loadFunc(){
     document.querySelector('.program').classList.remove('d-none');
     document.querySelector('.program').style.animation = "programOpacity 1200ms forwards";
     loadingBar.classList.add('d-none');
+    callback();
   }, 1000)
+}
+
+function moveIcons(){
+  setTimeout(()=>{
+    document.querySelector('.icons-container a:nth-child(1) i').style.transform = "translateX(110px) rotateZ(0deg)";
+    document.querySelector('.icons-container a:nth-child(1) i').style.opacity = "1";
+  }, 1200);
+  setTimeout(()=>{
+    document.querySelector('.icons-container a:nth-child(2) i').style.transform = "translateX(110px) rotateZ(0deg)";
+    document.querySelector('.icons-container a:nth-child(2) i').style.opacity = "1";
+  }, 1500);
 }
